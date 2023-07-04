78d15d2f9b511e40fd57f1507ae76dd54b1a4be2
Paulo Henrique <contato@paulohenriquesn.com>
2023-07-04 16:09:38 -0300

    feat: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,29 +1,38 @@
 import { InvalidParamError } from "../errors/invalid-param.error";
 import { MissingParamError } from "../errors/missing-param-error";
-import { BadRequest } from "../helpers/http-helper";
+import { BadRequest, InternalServerError } from "../helpers/http-helper";
 import { Controller } from "../protocols/controller";
 import { EmailValidator } from "../protocols/email-validator";
 import { HttpRequest, HttpResponse } from "../protocols/http";
 
 export class SignUpController implements Controller {
-  private readonly emailValidator: EmailValidator
+  private readonly emailValidator: EmailValidator;
 
   constructor(emailValidator: EmailValidator) {
     this.emailValidator = emailValidator;
   }
-  
+
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFileds = ['name', 'email', 'password', 'passwordConfirmation']
-    for(const field of requiredFileds) {
-      if (!httpRequest.body[field]) {
-        return BadRequest(new MissingParamError(field))
+    try {
+      const requiredFileds = [
+        "name",
+        "email",
+        "password",
+        "passwordConfirmation",
+      ];
+      for (const field of requiredFileds) {
+        if (!httpRequest.body[field]) {
+          return BadRequest(new MissingParamError(field));
+        }
       }
-    }
 
-    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      const isValid = this.emailValidator.isValid(httpRequest.body.email);
 
-    if(!isValid) {
-      return BadRequest(new InvalidParamError('email'))
+      if (!isValid) {
+        return BadRequest(new InvalidParamError("email"));
+      }
+    } catch (error) {
+      return InternalServerError();
     }
   }
 }
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,6 +1,13 @@
+import { ServerError } from "../errors/server-error";
 import { HttpResponse } from "../protocols/http";
 
 export const BadRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error,
 });
+
+
+export const InternalServerError = (): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError()
+}) 
