dddc7119868601394019442f3a881f61d8b0a454
Paulo Henrique <contato@paulohenriquesn.com>
2023-07-05 21:55:10 -0300

    feat: ensure SignUpController call addAccount with correct values

---
+++ src/presentation/controllers/signup/signup.ts
@@ -0,0 +1,56 @@
+import { InvalidParamError, MissingParamError } from '../../errors'
+import { BadRequest, InternalServerError } from '../../helpers/http-helper'
+import { type AddAccount, type Controller, type EmailValidator, type HttpRequest, type HttpResponse } from './signup-protocols'
+
+export class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator
+  private readonly addAccount: AddAccount
+
+  constructor (emailValidator: EmailValidator, addAccount: AddAccount) {
+    this.emailValidator = emailValidator
+    this.addAccount = addAccount
+  }
+
+  handle (httpRequest: HttpRequest): HttpResponse {
+    try {
+      const requiredFileds = [
+        'name',
+        'email',
+        'password',
+        'passwordConfirmation'
+      ]
+      for (const field of requiredFileds) {
+        if (!httpRequest.body[field]) {
+          return BadRequest(new MissingParamError(field))
+        }
+      }
+
+      const { password, passwordConfirmation, email, name } = httpRequest.body
+
+      if (password !== passwordConfirmation) {
+        return BadRequest(new InvalidParamError('passwordConfirmation'))
+      }
+
+      const isValid = this.emailValidator.isValid(email)
+
+      if (!isValid) {
+        return BadRequest(new InvalidParamError('email'))
+      }
+
+      this.addAccount.add({
+        name,
+        email,
+        password
+      })
+
+      return {
+        statusCode: 200,
+        body: {
+          message: 'User created successfully'
+        }
+      }
+    } catch (error) {
+      return InternalServerError()
+    }
+  }
+}
